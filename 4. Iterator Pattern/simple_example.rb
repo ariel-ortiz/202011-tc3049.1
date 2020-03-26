c6cb6c17d4dadafe8443c0f393e0ba31bc49463d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-03-26 16:20:11 +0000

    Solution to class exercises: pow2 and pow2_infinity

--- 4. Iterator Pattern/simple_example.rb
+++ 4. Iterator Pattern/simple_example.rb
@@ -8,8 +8,29 @@ def make_generator(n)
   end
 end
 
-g = make_generator(5)
-puts g.next  # 5
-puts g.next  # 6
-puts g.next  # 16
-puts g.next  # StopIteration
+def pow2(n)
+  Enumerator.new do |yielder|
+    n.times do |i|
+      yielder << 2 ** i
+    end
+  end
+end
+
+def pow2_infinity
+  Enumerator.new do |yielder|
+    n = 1
+    loop do
+      yielder << n
+      n *= 2
+    end
+  end
+end
+
+g1 = make_generator(5)
+p g1.to_a
+
+g2 = pow2(5)
+p g2.to_a
+
+g3 = pow2_infinity
+p g3.take(10)
